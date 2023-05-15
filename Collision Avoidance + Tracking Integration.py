3a54a7696245cdd808f392be2df8891747c187e9
emilyhoa <129704407+emilyhoa@users.noreply.github.com>
2023-05-15 12:13:53 -0700

    Update and rename Collision Avoidance + Tracking Integration to Collision Avoidance + Tracking Integration.py

--- Collision Avoidance + Tracking Integration.py
+++ Collision Avoidance + Tracking Integration.py
@@ -17,46 +17,7 @@ GPIO.setmode(GPIO.BCM)
 class drone(StateMachine):
 
     # sequence of events
-
-    def start(self):
-        """
-        LAUNCH PLAN:
-        initialize sensors + camera
-        zero flight controller altitude
-        rise to designated height
-        detect april tag
-        if successfully detected, move to tracking
-        """
-
-        master = mavutil.mavlink_connection("/dev/ttyACM0", baud=xxx) # Need serial port + baudrate
-        master.reboot_autopilot() # reboots ArduSub board
-
-        master = mavutil.mavlink_connection('udpin:0.0.0.0:9000')
-        master.wait_heartbeat()
-        print("Heartbeat from system (system %u component %u)" %
-              (master.target_system, master.target_component))
-
-        """
-        Do we need this? idk
-        # Send a message for QGC to read out loud
-        # Severity from https://mavlink.io/en/messages/common.html#MAV_SEVERITY
-        master.mav.statustext_send(mavutil.mavlink.MAV_SEVERITY_NOTICE,
-                                   "QGC will read this".encode())
-        """
-
-        # Arming vehicle
-        master.mav.command_long_send(master.target_system,
-                                     master.target_component,
-                                     mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-                                     0,
-                                     1, 0, 0, 0, 0, 0, 0)
-
-        # wait until arming confirmed (can manually check with master.motors_armed())
-        print("Waiting for the vehicle to arm")
-        master.motors_armed_wait()
-        print('Armed!')
-
-
+    
     def tracking(self):
         """
         TRACKING PLAN:
@@ -182,9 +143,36 @@ class drone(StateMachine):
                     right = distance(R) # motitor current obstacle
                     print("move foward")  
                 #Print("Transition to tracking)
-    except KeyboardInterrupt:
-        print("Measurement stopped by user")
-        GPIO.cleanup()
+        except KeyboardInterrupt:
+            print("Measurement stopped by user")
+            GPIO.cleanup()
+        def distance(GPIOpin):#Sort distance according to GPIOpin, labeled as FOWARD, LEFT, RIGHT, and REAR in pseudocode 
+            GPIO_SIG = GPIOpin;
+            GPIO.setup(GPIO_SIG, GPIO.OUT)
+            GPIO.output(GPIO_SIG, 0)
+
+            time.sleep(0.000002)
+
+            #send trigger signal
+            GPIO.output(GPIO_SIG, 1)
+
+            time.sleep(0.000005)
+
+            GPIO.output(GPIO_SIG, 0)
+
+            GPIO.setup(GPIO_SIG, GPIO.IN)
+
+            while GPIO.input(GPIO_SIG) == 0:
+                starttime = time.time()
+
+            while GPIO.input(GPIO_SIG) == 1:
+                endtime = time.time()
+
+            duration = endtime - starttime
+            # Distance is defined as time/2 (there and back) * speed of sound 34000 cm/s 
+            distance = (duration*34000)/2/100 #return in meters
+
+            return distance
         
     def tagnotdetected(self):
         """
@@ -209,31 +197,4 @@ class drone(StateMachine):
                 # transition to tracking state
 
         print("Transition to landing state")
-        
-def distance(GPIOpin):#Sort distance according to GPIOpin, labeled as FOWARD, LEFT, RIGHT, and REAR in pseudocode 
-    GPIO_SIG = GPIOpin;
-    GPIO.setup(GPIO_SIG, GPIO.OUT)
-    GPIO.output(GPIO_SIG, 0)
-
-    time.sleep(0.000002)
-
-    #send trigger signal
-    GPIO.output(GPIO_SIG, 1)
-
-    time.sleep(0.000005)
-
-    GPIO.output(GPIO_SIG, 0)
-
-    GPIO.setup(GPIO_SIG, GPIO.IN)
-
-    while GPIO.input(GPIO_SIG) == 0:
-        starttime = time.time()
-
-    while GPIO.input(GPIO_SIG) == 1:
-        endtime = time.time()
-      
-    duration = endtime - starttime
-    # Distance is defined as time/2 (there and back) * speed of sound 34000 cm/s 
-    distance = (duration*34000)/2/100 #return in meters
- 
-    return distance
+       
