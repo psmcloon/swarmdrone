42e428b0e3ec7cc7ff117cec67e708ac82fa75b0
kohyak <72419769+kohyak@users.noreply.github.com>
2023-05-15 11:53:39 -0700

    Add files via upload

---
+++ ApriltagNotDetected.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# import libraries
+import time
+
+for x in range(20):
+    time.sleep(1)
+    position[2] = 0
+    
+    # Mavlink command
+    print('hold position')
+    
+    # search for april tag
+    
+    
+    if position[2] == 0:
+        # transition to tracking state
+
+print("Transition to landing state")
+
